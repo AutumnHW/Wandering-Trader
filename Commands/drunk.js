20e74fc72caa5339b8081657c7067fa52f89799b
brandonhdyc <63067844+brandonhdyc@users.noreply.github.com>
2020-12-31 21:56:10 -0800

    added this wonderful... thing
    Written while drunk on a shot of 12% alcahol by volume wine. Fucking smashed LMAO

---
+++ Commands/drunk.js
@@ -0,0 +1,15 @@
+module.exports = {
+	name: 'drunk',
+	description: 'Wrote this while tipsy LMAO',
+	execute(message, args) {
+		message.reply('Fuck you initiating ban');
+		let fuckingyourmom = message.author.id;
+		let atmaker = message.mentions.users.first();
+		message.author.send('bruh')
+		message.author.send('https://cdn.discordapp.com/attachments/726960334350450689/783720625122181140/video0_1.mp4')
+		message.author.send('https://cdn.discordapp.com/attachments/726960334350450689/783016241211113532/hotroblox_1.mp4')
+		message.author.send('shawtys like a melody in myyyyyyyyyyyyyyyyyyyyy')
+		console.log(atmaker)
+		
+	},
+};
