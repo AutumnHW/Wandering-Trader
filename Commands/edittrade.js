6af4e7d67c8a5cdee827712b85147d93e39f6f63
spinningtoilet <71576099+spinningtoilet0@users.noreply.github.com>
2021-01-02 17:06:24 -0500

    Update edittrade.js

--- Commands/edittrade.js
+++ Commands/edittrade.js
@@ -8,29 +8,31 @@ module.exports = {
 			function readWriteAsync() {
 				const filePath = '../orders/' + args[0] + '.json';
 				const { order, requester, requestee, want, give, accepted, completed, private } = require(filePath);
-				if (!(message.author.id == requester) || !(message.author.id == requestee)) {
+				if (message.author.id == requester || message.author.id == requestee) {
+					var fileToEdit = "./orders/" + args[0] + ".json";
+					fs.readFile(fileToEdit, 'utf-8', function (err, data) {
+						if (err) throw err;
+						var toEdit = '"want": "' + want + '",';
+						var toEdit2 = '"want": "' + args[2] + '",';
+						var newValue = data.replace(toEdit, toEdit2);
+	
+						fs.writeFile(fileToEdit, newValue, 'utf-8', function (err) {
+							if (err) throw err;
+							console.log('changed file ' + filePath);
+							message.reply('done, the new order data should be sent to you.');
+							message.client.users.fetch(requester, false).then((user) => {
+								user.send("Order: " + order + ",\nI Want: " + want + ",\nFor: " + give + ",\nAccepted: " + accepted + ",\nCompleted: " + completed + ",\nPrivate: " + private + ".\n\n\nThat is your order data, now updated.");
+							});
+							message.client.users.fetch(requestee, false).then((user) => {
+								user.send("Order: " + order + ",\nI Want: " + want + ",\nFor: " + give + ",\nAccepted: " + accepted + ",\nCompleted: " + completed + ",\nPrivate: " + private + ".\n\n\nThat is your order data, now updated.");
+							});
+						});
+					});
+				}
+				else {
 					message.channel.send('You are not authorised to edit this transaction.');
 					return;
 				}
-				var fileToEdit = "./orders/" + args[0] + ".json";
-				fs.readFile(fileToEdit, 'utf-8', function (err, data) {
-					if (err) throw err;
-					var toEdit = '"want": "' + want + '",';
-					var toEdit2 = '"want": "' + args[2] + '",';
-					var newValue = data.replace(toEdit, toEdit2);
-
-					fs.writeFile(fileToEdit, newValue, 'utf-8', function (err) {
-						if (err) throw err;
-						console.log('changed file ' + filePath);
-						message.reply('done, the new order data should be sent to you.');
-						message.client.users.fetch(requester, false).then((user) => {
-							user.send("Order: " + order + ",\nI Want: " + want + ",\nFor: " + give + ",\nAccepted: " + accepted + ",\nCompleted: " + completed + ",\nPrivate: " + private + ".\n\n\nThat is your order data, now updated.");
-						});
-						message.client.users.fetch(requestee, false).then((user) => {
-							user.send("Order: " + order + ",\nI Want: " + want + ",\nFor: " + give + ",\nAccepted: " + accepted + ",\nCompleted: " + completed + ",\nPrivate: " + private + ".\n\n\nThat is your order data, now updated.");
-						});
-					});
-				});
 			}
 
 			readWriteAsync();
@@ -39,29 +41,31 @@ module.exports = {
 			function readWriteAsync() {
 				const filePath = '../orders/' + args[0] + '.json';
 				const { order, requester, requestee, want, give, accepted, completed, private } = require(filePath);
-				if (!(message.author.id == requester) || !(message.author.id == requestee)) {
+				if (message.author.id == requester || message.author.id == requestee) {
+					var fileToEdit = "./orders/" + args[0] + ".json";
+					fs.readFile(fileToEdit, 'utf-8', function (err, data) {
+						if (err) throw err;
+						var toEdit = '"give": "' + give + '",';
+						var toEdit2 = '"give": "' + args[2] + '",';
+						var newValue = data.replace(toEdit, toEdit2);
+	
+						fs.writeFile(fileToEdit, newValue, 'utf-8', function (err) {
+							if (err) throw err;
+							console.log('changed file ' + filePath);
+							message.reply('done, the new order data should be sent to you.');
+							message.client.users.fetch(requester, false).then((user) => {
+								user.send("Order: " + order + ",\nI Want: " + want + ",\nFor: " + give + ",\nAccepted: " + accepted + ",\nCompleted: " + completed + ",\nPrivate: " + private + ".\n\n\nThat is your order data, now updated.");
+							});
+							message.client.users.fetch(requestee, false).then((user) => {
+								user.send("Order: " + order + ",\nI Want: " + want + ",\nFor: " + give + ",\nAccepted: " + accepted + ",\nCompleted: " + completed + ",\nPrivate: " + private + ".\n\n\nThat is your order data, now updated.");
+							});
+						});
+					});
+				}
+				else {
 					message.channel.send('You are not authorised to edit this transaction.');
 					return;
 				}
-				var fileToEdit = "./orders/" + args[0] + ".json";
-				fs.readFile(fileToEdit, 'utf-8', function (err, data) {
-					if (err) throw err;
-					var toEdit = '"give": "' + give + '",';
-					var toEdit2 = '"give": "' + args[2] + '",';
-					var newValue = data.replace(toEdit, toEdit2);
-
-					fs.writeFile(fileToEdit, newValue, 'utf-8', function (err) {
-						if (err) throw err;
-						console.log('changed file ' + filePath);
-						message.reply('done, the new order data should be sent to you.');
-						message.client.users.fetch(requester, false).then((user) => {
-							user.send("Order: " + order + ",\nI Want: " + want + ",\nFor: " + give + ",\nAccepted: " + accepted + ",\nCompleted: " + completed + ",\nPrivate: " + private + ".\n\n\nThat is your order data, now updated.");
-						});
-						message.client.users.fetch(requestee, false).then((user) => {
-							user.send("Order: " + order + ",\nI Want: " + want + ",\nFor: " + give + ",\nAccepted: " + accepted + ",\nCompleted: " + completed + ",\nPrivate: " + private + ".\n\n\nThat is your order data, now updated.");
-						});
-					});
-				});
 			}
 
 			readWriteAsync();
@@ -70,29 +74,31 @@ module.exports = {
 			function readWriteAsync() {
 				const filePath = '../orders/' + args[0] + '.json';
 				const { order, requester, requestee, want, give, accepted, completed, private } = require(filePath);
-				if (!(message.author.id == requester) || !(message.author.id == requestee)) {
+				if (message.author.id == requestee) {
+					var fileToEdit = "./orders/" + args[0] + ".json";
+					fs.readFile(fileToEdit, 'utf-8', function (err, data) {
+						if (err) throw err;
+						var toEdit = '"accepted": ' + accepted + ',';
+						var toEdit2 = '"accepted": ' + args[2] + ',';
+						var newValue = data.replace(toEdit, toEdit2);
+	
+						fs.writeFile(fileToEdit, newValue, 'utf-8', function (err) {
+							if (err) throw err;
+							console.log('changed file ' + filePath);
+							message.reply('done, the new order data should be sent to you.');
+							message.client.users.fetch(requester, false).then((user) => {
+								user.send("Order: " + order + ",\nI Want: " + want + ",\nFor: " + give + ",\nAccepted: " + accepted + ",\nCompleted: " + completed + ",\nPrivate: " + private + ".\n\n\nThat is your order data, now updated.");
+							});
+							message.client.users.fetch(requestee, false).then((user) => {
+								user.send("Order: " + order + ",\nI Want: " + want + ",\nFor: " + give + ",\nAccepted: " + accepted + ",\nCompleted: " + completed + ",\nPrivate: " + private + ".\n\n\nThat is your order data, now updated.");
+							});
+						});
+					});
+				}
+				else {
 					message.channel.send('You are not authorised to edit this transaction.');
 					return;
 				}
-				var fileToEdit = "./orders/" + args[0] + ".json";
-				fs.readFile(fileToEdit, 'utf-8', function (err, data) {
-					if (err) throw err;
-					var toEdit = '"accepted": ' + accepted + ',';
-					var toEdit2 = '"accepted": ' + args[2] + ',';
-					var newValue = data.replace(toEdit, toEdit2);
-
-					fs.writeFile(fileToEdit, newValue, 'utf-8', function (err) {
-						if (err) throw err;
-						console.log('changed file ' + filePath);
-						message.reply('done, the new order data should be sent to you.');
-						message.client.users.fetch(requester, false).then((user) => {
-							user.send("Order: " + order + ",\nI Want: " + want + ",\nFor: " + give + ",\nAccepted: " + accepted + ",\nCompleted: " + completed + ",\nPrivate: " + private + ".\n\n\nThat is your order data, now updated.");
-						});
-						message.client.users.fetch(requestee, false).then((user) => {
-							user.send("Order: " + order + ",\nI Want: " + want + ",\nFor: " + give + ",\nAccepted: " + accepted + ",\nCompleted: " + completed + ",\nPrivate: " + private + ".\n\n\nThat is your order data, now updated.");
-						});
-					});
-				});
 			}
 
 			readWriteAsync();
@@ -101,29 +107,31 @@ module.exports = {
 			function readWriteAsync() {
 				const filePath = '../orders/' + args[0] + '.json';
 				const { order, requester, requestee, want, give, accepted, completed, private } = require(filePath);
-				if (!(message.author.id == requester) || !(message.author.id == requestee)) {
+				if (message.author.id == requester || message.author.id == requestee) {
+					var fileToEdit = "./orders/" + args[0] + ".json";
+					fs.readFile(fileToEdit, 'utf-8', function (err, data) {
+						if (err) throw err;
+						var toEdit = '"completed": ' + completed + ',';
+						var toEdit2 = '"completed": ' + args[2] + ',';
+						var newValue = data.replace(toEdit, toEdit2);
+	
+						fs.writeFile(fileToEdit, newValue, 'utf-8', function (err) {
+							if (err) throw err;
+							console.log('changed file ' + filePath);
+							message.reply('done, the new order data should be sent to you.');
+							message.client.users.fetch(requester, false).then((user) => {
+								user.send("Order: " + order + ",\nI Want: " + want + ",\nFor: " + give + ",\nAccepted: " + accepted + ",\nCompleted: " + completed + ",\nPrivate: " + private + ".\n\n\nThat is your order data, now updated.");
+							});
+							message.client.users.fetch(requestee, false).then((user) => {
+								user.send("Order: " + order + ",\nI Want: " + want + ",\nFor: " + give + ",\nAccepted: " + accepted + ",\nCompleted: " + completed + ",\nPrivate: " + private + ".\n\n\nThat is your order data, now updated.");
+							});
+						});
+					});
+				}
+				else {
 					message.channel.send('You are not authorised to edit this transaction.');
 					return;
 				}
-				var fileToEdit = "./orders/" + args[0] + ".json";
-				fs.readFile(fileToEdit, 'utf-8', function (err, data) {
-					if (err) throw err;
-					var toEdit = '"completed": ' + completed + ',';
-					var toEdit2 = '"completed": ' + args[2] + ',';
-					var newValue = data.replace(toEdit, toEdit2);
-
-					fs.writeFile(fileToEdit, newValue, 'utf-8', function (err) {
-						if (err) throw err;
-						console.log('changed file ' + filePath);
-						message.reply('done, the new order data should be sent to you.');
-						message.client.users.fetch(requester, false).then((user) => {
-							user.send("Order: " + order + ",\nI Want: " + want + ",\nFor: " + give + ",\nAccepted: " + accepted + ",\nCompleted: " + completed + ",\nPrivate: " + private + ".\n\n\nThat is your order data, now updated.");
-						});
-						message.client.users.fetch(requestee, false).then((user) => {
-							user.send("Order: " + order + ",\nI Want: " + want + ",\nFor: " + give + ",\nAccepted: " + accepted + ",\nCompleted: " + completed + ",\nPrivate: " + private + ".\n\n\nThat is your order data, now updated.");
-						});
-					});
-				});
 			}
 
 			readWriteAsync();
@@ -133,30 +141,31 @@ module.exports = {
 				const filePath = '../orders/' + args[0] + '.json';
 				const { order, requester, requestee, want, give, accepted, completed, private } = require(filePath);
 				if (!(message.author.id == requester) || !(message.author.id == requestee)) {
+					var fileToEdit = "./orders/" + args[0] + ".json";
+					fs.readFile(fileToEdit, 'utf-8', function (err, data) {
+						if (err) throw err;
+						var toEdit = '"private": ' + private;
+						var toEdit2 = '"private": ' + args[2];
+						var newValue = data.replace(toEdit, toEdit2);
+	
+						fs.writeFile(fileToEdit, newValue, 'utf-8', function (err) {
+							if (err) throw err;
+							const filePath = '../orders/' + args[0];
+							console.log('changed file ' + filePath);
+							message.reply('done, the new order data should be sent to you.');
+							message.client.users.fetch(requester, false).then((user) => {
+								user.send("Order: " + order + ",\nI Want: " + want + ",\nFor: " + give + ",\nAccepted: " + accepted + ",\nCompleted: " + completed + ",\nPrivate: " + private + ".\n\n\nThat is your order data, now updated.");
+							});
+							message.client.users.fetch(requestee, false).then((user) => {
+								user.send("Order: " + order + ",\nI Want: " + want + ",\nFor: " + give + ",\nAccepted: " + accepted + ",\nCompleted: " + completed + ",\nPrivate: " + private + ".\n\n\nThat is your order data, now updated.");
+							});
+						});
+					});	
+				}
+				else {
 					message.channel.send('You are not authorised to edit this transaction.');
 					return;
 				}
-				var fileToEdit = "./orders/" + args[0] + ".json";
-				fs.readFile(fileToEdit, 'utf-8', function (err, data) {
-					if (err) throw err;
-					var toEdit = '"private": ' + private;
-					var toEdit2 = '"private": ' + args[2];
-					var newValue = data.replace(toEdit, toEdit2);
-
-					fs.writeFile(fileToEdit, newValue, 'utf-8', function (err) {
-						if (err) throw err;
-						const filePath = '../orders/' + args[0];
-						console.log('changed file ' + filePath);
-						message.reply('done, the new order data should be sent to you.');
-						message.client.users.fetch(requester, false).then((user) => {
-							user.send("Order: " + order + ",\nI Want: " + want + ",\nFor: " + give + ",\nAccepted: " + accepted + ",\nCompleted: " + completed + ",\nPrivate: " + private + ".\n\n\nThat is your order data, now updated.");
-						});
-						message.client.users.fetch(requestee, false).then((user) => {
-							user.send("Order: " + order + ",\nI Want: " + want + ",\nFor: " + give + ",\nAccepted: " + accepted + ",\nCompleted: " + completed + ",\nPrivate: " + private + ".\n\n\nThat is your order data, now updated.");
-						});
-					});
-				});
-
 			}
 
 			readWriteAsync();
