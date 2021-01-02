3fae92f4384482147133b9a9e588a8b9992dbb88
brandonhdyc <63067844+brandonhdyc@users.noreply.github.com>
2021-01-02 14:38:41 -0500

    fixed syntax issue
    ha missing comma go brrrrrrrrrrrrrr

--- Commands/help.js
+++ Commands/help.js
@@ -7,8 +7,9 @@ module.exports = {
         .setColor('#04910e')
         .setTitle('Help')
        .addFields(
-           {name: '```t!tr```', value: 'Opens a trade request, Must mention someone in the command. Example:```t!tr <exampleuser> <trade contents>``` (-p if you want to set private, private trades do not appear in the trade log channel)'}
+           {name: '```t!tr```', value: 'Opens a trade request, Must mention someone in the command. Example:```t!tr <exampleuser> <trade contents>``` (-p if you want to set private, private trades do not appear in the trade log channel)'},
            {name: '```t!et```', value: 'Allows for you to accept/deny a trade request, as well as update order contents. Example:```t!et <settingtochange> <contentyouwant>```'}
+           
        )
 
         ;
